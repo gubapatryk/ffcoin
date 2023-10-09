91cbbf6cef3d65abdbaf730a3247562665a75fd6
liath <matthewdragun@gmail.com>
2023-10-10 00:11:55 +0200

    added cli command for printing peers

--- cli/cli_definitions.py
+++ cli/cli_definitions.py
@@ -1,11 +1,13 @@
 from cli.cli_option import CLIOption
 from commands.registration import register
+from commands.state import print_user_peers
 from commands.wallet import set_up_keys, load_keys
 
 cli_options = [
   CLIOption("1", "Create key pair", set_up_keys),
   CLIOption("2", "Load key pair", load_keys),
   CLIOption("3", "Broadcast greetings to known peers", register),
+  CLIOption("4", "List peers", print_user_peers),
   CLIOption("exit", "Exit", lambda _: exit())
 ]
 
---
+++ commands/state.py
@@ -0,0 +1,3 @@
+def print_user_peers(state):
+  for peer in state.peers:
+    print(f" > {peer}")

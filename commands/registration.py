e77e66be14792bcdd60e2f1afbd23bcf6a7ef4d6
liath <matthewdragun@gmail.com>
2023-10-22 18:15:26 +0200

    debug log

--- commands/registration.py
+++ commands/registration.py
@@ -15,6 +15,10 @@ def greet_peers(state: State):
   ip: str
   user: User
   for ip, user in state.peers.copy().items():
+    print("IP to greet")
+    print(ip)
+    print("USER to greet")
+    print(user)
     if user.public_key is not None:
       # blocking
       out = requests.post(

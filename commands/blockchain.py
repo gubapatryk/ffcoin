fc8fd055c280afd17bee1002d926036f2cb376f6
nikt <gubapatryk@gmail.com>
2023-11-21 18:21:07 +0000

    eh

--- commands/blockchain.py
+++ commands/blockchain.py
@@ -7,6 +7,7 @@ import jsonpickle
 def print_blockchain(state):
     state.blockchain.display_blocks()
 
+
 def add_data_blockchain(state):
     data = input("Provide data to save in blockchain: ").strip()
     state.blockchain.add_block_with_data(data)

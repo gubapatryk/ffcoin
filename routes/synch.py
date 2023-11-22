1dd4f0b6145e4d6f7669969850c43330c07546bf
pg <gubapatryk@gmail.com>
2023-11-22 08:46:53 +0000

    validation

--- routes/synch.py
+++ routes/synch.py
@@ -8,14 +8,18 @@ from constants import HTTP_CONSTANTS, PORT, JSON_CONSTANTS, IP
 from flask_app import flask_app
 from flask import request, Response
 
+from commands.blockchain import is_good_new_blockchain
 
 @flask_app.route("/synch", methods=["POST"])
 def synch():
-  state.blockchain.chain = jsonpickle.decode(request.get_json())
-  print(request.get_json())
-  print("lets goooo")
-  print(state.blockchain)
-  return {}
+  new_bc = jsonpickle.decode(request.get_json())
+  if is_good_new_blockchain(state.blockchain.chain,new_bc):
+    state.blockchain.chain = new_bc
+    return "true"
+  return "false"
+
+
+
 
 @flask_app.route("/synch", methods=["GET"])
 def send_chain():

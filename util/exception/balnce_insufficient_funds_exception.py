c96e2c4bf3e060fe4175f35f4e98a105989cd939
liath <matthewdragun@gmail.com>
2023-12-13 02:29:58 +0100

    trying to fix circular import

--- util/exception/balnce_insufficient_funds_exception.py
+++ util/exception/balnce_insufficient_funds_exception.py
@@ -1,7 +1,4 @@
-from state import User
-
-
 class BalanceInsufficientFundsException(Exception):
-  def __init__(self, user: User, amount: float):
+  def __init__(self, user, amount: float):
     self.msg = f"User {user.name} has insufficient funds for operation of {amount}. Blockchain is incorrect"
     super().__init__(self.msg)
